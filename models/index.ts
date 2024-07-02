833a5e7fbc9777bd3bcde417eee48d173a1968f6
katta soumya <kattasoumya2002@gmail.com>
2024-07-02 18:32:19 +0530

    take a static data

--- models/index.ts
+++ models/index.ts
@@ -1,5 +1,12 @@
 const { Sequelize } = require('sequelize');
 import { userModel } from "./user";
+const data = [
+  { firstName: "soumya", lastName: "katta" },
+  { firstName: "sakshi", lastName: "katta" },
+  { firstName: "tej", lastName: "lalugani" },
+  { firstName: "kiya", lastName: "bejjarapu" },
+  { firstName: "sumith", lastName: "bejjarapu"},
+]
 const Connection = async () => {
   const sequelize = new Sequelize("school_db", "postgres", "1405", {
     host: "localhost",
@@ -11,6 +18,7 @@ const Connection = async () => {
     console.log("Connection has been established successfully.");
     await userModel(sequelize).sync({ force: true });
     console.log("The table for the User model was just (re)created!");
+    const users = await userModel(sequelize).bulkCreate(data);
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
