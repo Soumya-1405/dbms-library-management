26594a33d4644f0fece33ce0296bdf0dc1cb93b4
katta soumya <kattasoumya2002@gmail.com>
2024-07-02 14:28:35 +0530

    sync user table to sequelize

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 const { Sequelize } = require('sequelize');
 // import { userModel } from "./user";
-import { User } from "./user";
+import { userModel } from "./user";
 const Connection = async () => {
   const sequelize = new Sequelize("school_db", "postgres", "1405", {
     host: "localhost",
     dialect: "postgres",
     port: 5433,
   });
+  let User = null;
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    // User = userModel(sequelize);
-    await User.sync({ force: true });
+    
+    await userModel(sequelize).sync({ force: true });
     console.log("The table for the User model was just (re)created!");
-    // console.log("The table for the User model was just (re)created!");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
