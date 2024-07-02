595c2746e28fdd6f8abacccc23e490562132aaa9
katta soumya <kattasoumya2002@gmail.com>
2024-07-02 16:10:23 +0530

    create a models folder and keep all models inside of it

---
+++ models/user.ts
@@ -0,0 +1,24 @@
+const sequelize = require('./index.js');
+import { DataTypes } from "sequelize";
+export const userModel = (sequelize:any) => {
+    return sequelize.define(
+        'User',
+        {
+          // Model attributes are defined here
+          firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          lastName: {
+            type: DataTypes.STRING,
+            // allowNull defaults to true
+          },
+        },
+        {
+          // Other model options go here
+          timestamps: false,
+        },
+      );
+}
+
+
