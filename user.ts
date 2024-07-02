86c6c7f1b2eda9c69923bc077df32a73450a218a
katta soumya <kattasoumya2002@gmail.com>
2024-07-02 14:27:24 +0530

    remove the commented statements

--- user.ts
+++ user.ts
@@ -1,50 +1,23 @@
-// // const { Sequelize, DataTypes } = require('sequelize');
-// import { Sequelize } from "sequelize";
-// // import { DataTypes } from "sequelize";
-// // const sequelize = new Sequelize('sqlite::memory:');
-// export const userModel = (sequelize:any) => {
-//     const {DataTypes} = sequelize;
-//     return sequelize.define(
-//         'User',
-//         {
-//           // Model attributes are defined here
-//           firstName: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//           },
-//           lastName: {
-//             type: DataTypes.STRING,
-//             // allowNull defaults to true
-//           },
-//         },
-//         {
-//           // Other model options go here
-//         },
-//       );
-// }
-
-
-// // // `sequelize.define` also returns the model
-// // console.log(User === sequelize.models.User); // true
-
 const sequelize = require('./index.js');
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
+export const userModel = (sequelize:any) => {
+    return sequelize.define(
+        'User',
+        {
+          // Model attributes are defined here
+          firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          lastName: {
+            type: DataTypes.STRING,
+            // allowNull defaults to true
+          },
+        },
+        {
+          // Other model options go here
+        },
+      );
+}
 
-/**
- * User model
- */
-export const User = sequelize.define('user', {
 
-  id: {
-    type: DataTypes.INTEGER,
-  },
-  name: {
-    type: DataTypes.STRING,
-  },
-  email: {
-    type: DataTypes.STRING,
-  },
-  lastEmail: {
-    type: DataTypes.TIME,
-  }
-});
