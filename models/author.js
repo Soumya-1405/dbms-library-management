9fe394c41bfe1df7bfc78a30a11913b1caeb2b76
katta soumya <kattasoumya2002@gmail.com>
2024-07-03 14:10:43 +0530

    update the author js file

--- models/author.js
+++ models/author.js
@@ -1,27 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.authorModel = void 0;
-var sequelize = require("./index.js");
+exports.Author = void 0;
+var db_connection_1 = require("../db_connection");
 var sequelize_1 = require("sequelize");
-var authorModel = function (sequelize) {
-    return sequelize.define("Author", {
-        id: {
-            type: sequelize_1.DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: sequelize_1.DataTypes.STRING,
-        },
-        birth_year: {
-            type: sequelize_1.DataTypes.INTEGER,
-        },
-        nationality: {
-            type: sequelize_1.DataTypes.STRING,
-        },
-    }, {
-        timestamps: false,
-    });
-};
-exports.authorModel = authorModel;
+exports.Author = db_connection_1.default.define("Author", {
+    id: {
+        type: sequelize_1.DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    birth_year: {
+        type: sequelize_1.DataTypes.INTEGER,
+    },
+    nationality: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+}, {
+    timestamps: false,
+}, {
+    tableName: 'Books'
+});
