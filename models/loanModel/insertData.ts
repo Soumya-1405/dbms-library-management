837cae7b251c624986b473970f52e5313c248c9b
katta soumya <kattasoumya2002@gmail.com>
2024-07-04 01:37:26 +0530

    insert data into loans table

--- models/loanModel/insertData.ts
+++ models/loanModel/insertData.ts
@@ -6,36 +6,41 @@ import { DataTypes } from "sequelize";
 const loansData = [
   {
     id: 1,
+    book_id:2,
     member_id: 2,
-    // loan_date: 2,
-    // due_date: "gen1",
+    loan_date: new Date(),
+    due_date: new Date(new Date().setDate(new Date().getDate() + 14)),
   },
   {
     id: 2,
+    book_id:1,
     member_id: 3,
-    // loan_date: Dat,
-    // due_date: "gen2",
+    loan_date: new Date(),
+    due_date: new Date(new Date().setDate(new Date().getDate() + 14)),
   },
   {
     id: 3,
+    book_id:4,
     member_id: 4,
-    // loan_date: 4,
-    // due_date: "gen3",
+    loan_date: new Date(),
+    due_date: new Date(new Date().setDate(new Date().getDate() + 14)),
   },
   {
     id: 4,
+    book_id:3,
     member_id: 5,
-    // loan_date: 5,
-    // due_date: "gen4",
+    loan_date: new Date(),
+    due_date: new Date(new Date().setDate(new Date().getDate() + 14)),
   },
   {
     id: 5,
+    book_id:5,
     member_id: 1,
-    // loan_date: 1,
-    // due_date: "gen5"
+    loan_date: new Date(),
+    due_date: new Date(new Date().setDate(new Date().getDate() + 14)),
   },
 ];
 
 export const insertLoansData = async () => {
-  const books = await Loan.bulkCreate(loansData);
+  const loans = await Loan.bulkCreate(loansData);
 };
