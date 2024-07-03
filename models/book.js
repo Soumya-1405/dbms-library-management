b94dcb5482306191ff711b9f89cd1f291e589567
katta soumya <kattasoumya2002@gmail.com>
2024-07-03 14:05:50 +0530

    update the book js file

--- models/book.js
+++ models/book.js
@@ -1,35 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.bookModel = void 0;
-var sequelize = require('./index.js');
+exports.Book = void 0;
+var sequelize = require("../db_connection");
 var sequelize_1 = require("sequelize");
-var bookModel = function (sequelize) {
-    return sequelize.define('Book', {
-        id: {
-            type: sequelize_1.DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        title: {
-            type: sequelize_1.DataTypes.STRING,
-        },
-        authorId: {
-            type: sequelize_1.DataTypes.INTEGER,
-            // references: 'Authors',
-            // referencesKey: 'id'
-        },
-        genre: {
-            type: sequelize_1.DataTypes.STRING,
-        },
-        isbn: {
-            type: sequelize_1.DataTypes.STRING,
-        },
-        publication_year: {
-            type: sequelize_1.DataTypes.INTEGER,
-        }
-    }, {
-        timestamps: false,
-    });
-};
-exports.bookModel = bookModel;
+exports.Book = sequelize.define('Book', {
+    id: {
+        type: sequelize_1.DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    authorId: {
+        type: sequelize_1.DataTypes.INTEGER,
+        // references: 'Authors',
+        // referencesKey: 'id'
+    },
+    genre: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    isbn: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    publication_year: {
+        type: sequelize_1.DataTypes.INTEGER,
+    }
+}, {
+    timestamps: false,
+}, {
+    tableName: 'Books'
+});
