4c42072cfe95d968e6231eeeb083e7762f5a4ffd
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 01:24:03 +0530

    place all models in model folder

---
+++ models/book.ts
@@ -0,0 +1,43 @@
+// import sequelize from "../db_connection/index";
+import { Sequelize, DataTypes } from "sequelize";
+import { Author } from "./author";
+
+import {sequelize} from '../db_connection/config'
+
+export const Book = sequelize.define(
+        'Book',
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          title: {
+            type: DataTypes.STRING,
+          },
+          authorId:{
+            type: DataTypes.INTEGER,
+            references: {
+              model: Author,
+              key: 'id'
+          }
+          },
+          genre:{
+            type: DataTypes.STRING,
+          },
+          isbn:{
+            type: DataTypes.STRING,
+          },
+          publication_year:{
+            type: DataTypes.INTEGER,
+          }
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      Author.hasMany(Book,{foreignKey: 'authorId'});
+      Book.belongsTo(Author,{foreignKey: 'authorId'});
+
