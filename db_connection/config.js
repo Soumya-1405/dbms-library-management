1581acaf0ff93181bc667556a7d0238533c48414
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 00:37:21 +0530

    update config file

--- db_connection/config.js
+++ db_connection/config.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const { Sequelize } = require("sequelize");
@@ -6,6 +15,16 @@ const sequelize = new Sequelize("school_db", "postgres", "1405", {
     host: "localhost",
     dialect: "postgres",
     port: 5433,
-    logging: false
+    logging: false,
 });
 exports.sequelize = sequelize;
+const Connection = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    }
+    catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+});
+Connection();
--- db_connection/config.ts
+++ db_connection/config.ts
@@ -1,9 +1,18 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("school_db", "postgres", "1405", {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5433,
-    logging:false
-  });
-  
-export {sequelize}
+  host: "localhost",
+  dialect: "postgres",
+  port: 5433,
+  logging: false,
+});
+
+const Connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+Connection();
+export { sequelize };
