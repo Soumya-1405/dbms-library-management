4df53b7571dde8d01ec5a5cb207bcf05fec1517a
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 11:56:36 +0530

    perform crud opertaions on books table

--- Repository/book.repository.js
+++ Repository/book.repository.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertBooksData = void 0;
+exports.deleteBook = exports.updateBook = exports.getAllBooks = exports.insertBooksData = void 0;
 const book_1 = require("../models/book");
 const data_1 = require("../staticData/data");
 const insertBooksData = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -17,3 +17,47 @@ const insertBooksData = () => __awaiter(void 0, void 0, void 0, function* () {
     console.log("insert successfully");
 });
 exports.insertBooksData = insertBooksData;
+const getAllBooks = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const authors = yield book_1.Book.findAll();
+        console.table(authors.map((author) => author.toJSON()));
+    }
+    catch (error) {
+        console.log("error fetchiing authors", error);
+    }
+});
+exports.getAllBooks = getAllBooks;
+const updateBook = (authorId, updatedData) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield book_1.Book.findByPk(authorId);
+        if (author) {
+            yield author.update(updatedData);
+            const authors = yield book_1.Book.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Book not Found");
+        }
+    }
+    catch (error) {
+        console.log("error updating data", error);
+    }
+});
+exports.updateBook = updateBook;
+const deleteBook = (authorId) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield book_1.Book.findByPk(authorId);
+        if (author) {
+            yield author.destroy();
+            const authors = yield book_1.Book.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Book not Found");
+        }
+    }
+    catch (error) {
+        console.log("error deleteing data", error);
+    }
+});
+exports.deleteBook = deleteBook;
--- Repository/book.repository.ts
+++ Repository/book.repository.ts
@@ -1,7 +1,47 @@
 import { Book } from "../models/book";
 import { Data } from "../staticData/data";
+
 export const insertBooksData = async () => {
     const books = await Book.bulkCreate(Data.booksData);
     console.log("insert successfully");
+  };
+  
+  export const getAllBooks = async () => {
+    try {
+      const authors = await Book.findAll();
+      console.table(authors.map((author: any) => author.toJSON()));
+    } catch (error) {
+      console.log("error fetchiing authors", error);
+    }
+  };
+  
+  export const updateBook = async (authorId: Number, updatedData: Object) => {
+    try {
+      const author = await Book.findByPk(authorId);
+      if (author) {
+        await author.update(updatedData);
+        const authors = await Book.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log( "Book not Found");
+      }
+    } catch (error) {
+      console.log("error updating data", error);
+    }
+  };
+  
+  export const deleteBook = async (authorId: Number) => {
+    try {
+      const author = await Book.findByPk(authorId);
+      if (author) {
+        await author.destroy();
+        const authors = await Book.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Book not Found");
+      }
+    } catch (error) {
+      console.log("error deleteing data", error);
+    }
   };
   
