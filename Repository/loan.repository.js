86c8c4b59153398e6b5f113785b6d23052f12c5b
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 13:19:22 +0530

    perform crud operations on loans table

--- Repository/loan.repository.js
+++ Repository/loan.repository.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertLoansData = void 0;
+exports.deleteLoan = exports.updateLoan = exports.getAllLoans = exports.insertLoansData = void 0;
 const loan_1 = require("../models/loan");
 const data_1 = require("../staticData/data");
 const insertLoansData = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -17,3 +17,47 @@ const insertLoansData = () => __awaiter(void 0, void 0, void 0, function* () {
     console.log('insert succesfully');
 });
 exports.insertLoansData = insertLoansData;
+const getAllLoans = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const authors = yield loan_1.Loan.findAll();
+        console.table(authors.map((author) => author.toJSON()));
+    }
+    catch (error) {
+        console.log("error fetchiing authors", error);
+    }
+});
+exports.getAllLoans = getAllLoans;
+const updateLoan = (authorId, updatedData) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield loan_1.Loan.findByPk(authorId);
+        if (author) {
+            yield author.update(updatedData);
+            const authors = yield loan_1.Loan.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Loan not Found");
+        }
+    }
+    catch (error) {
+        console.log("error updating data", error);
+    }
+});
+exports.updateLoan = updateLoan;
+const deleteLoan = (authorId) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield loan_1.Loan.findByPk(authorId);
+        if (author) {
+            yield author.destroy();
+            const authors = yield loan_1.Loan.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Loan not Found");
+        }
+    }
+    catch (error) {
+        console.log("error deleteing data", error);
+    }
+});
+exports.deleteLoan = deleteLoan;
--- Repository/loan.repository.ts
+++ Repository/loan.repository.ts
@@ -5,3 +5,42 @@ export const insertLoansData = async () => {
     console.log('insert succesfully')
   };
   
+  export const getAllLoans = async () => {
+    try {
+      const authors = await Loan.findAll();
+      console.table(authors.map((author: any) => author.toJSON()));
+    } catch (error) {
+      console.log("error fetchiing authors", error);
+    }
+  };
+  
+  export const updateLoan = async (authorId: Number, updatedData: Object) => {
+    try {
+      const author = await Loan.findByPk(authorId);
+      if (author) {
+        await author.update(updatedData);
+        const authors = await Loan.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Loan not Found");
+      }
+    } catch (error) {
+      console.log("error updating data", error);
+    }
+  };
+  
+  export const deleteLoan = async (authorId: Number) => {
+    try {
+      const author = await Loan.findByPk(authorId);
+      if (author) {
+        await author.destroy();
+        const authors = await Loan.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Loan not Found");
+      }
+    } catch (error) {
+      console.log("error deleteing data", error);
+    }
+  };
+  
