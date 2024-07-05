604ca2248e0304934fb84062dc8e338cbdfa1fec
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 13:27:11 +0530

    perform crud operations on reservation table

--- Repository/reservation.repository.js
+++ Repository/reservation.repository.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertReservationData = void 0;
+exports.deleteReservation = exports.updateReservation = exports.getAllReservations = exports.insertReservationData = void 0;
 const reservation_1 = require("../models/reservation");
 const data_1 = require("../staticData/data");
 const insertReservationData = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -17,3 +17,47 @@ const insertReservationData = () => __awaiter(void 0, void 0, void 0, function* 
     console.log("insert successfully");
 });
 exports.insertReservationData = insertReservationData;
+const getAllReservations = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const authors = yield reservation_1.Reservation.findAll();
+        console.table(authors.map((author) => author.toJSON()));
+    }
+    catch (error) {
+        console.log("error fetchiing authors", error);
+    }
+});
+exports.getAllReservations = getAllReservations;
+const updateReservation = (authorId, updatedData) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield reservation_1.Reservation.findByPk(authorId);
+        if (author) {
+            yield author.update(updatedData);
+            const authors = yield reservation_1.Reservation.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Reservation not Found");
+        }
+    }
+    catch (error) {
+        console.log("error updating data", error);
+    }
+});
+exports.updateReservation = updateReservation;
+const deleteReservation = (authorId) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield reservation_1.Reservation.findByPk(authorId);
+        if (author) {
+            yield author.destroy();
+            const authors = yield reservation_1.Reservation.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Author not Found");
+        }
+    }
+    catch (error) {
+        console.log("error deleteing data", error);
+    }
+});
+exports.deleteReservation = deleteReservation;
--- Repository/reservation.repository.ts
+++ Repository/reservation.repository.ts
@@ -5,3 +5,42 @@ export const insertReservationData = async () => {
     console.log("insert successfully");
   };
   
+  export const getAllReservations = async () => {
+    try {
+      const authors = await Reservation.findAll();
+      console.table(authors.map((author: any) => author.toJSON()));
+    } catch (error) {
+      console.log("error fetchiing authors", error);
+    }
+  };
+  
+  export const updateReservation = async (authorId: Number, updatedData: Object) => {
+    try {
+      const author = await Reservation.findByPk(authorId);
+      if (author) {
+        await author.update(updatedData);
+        const authors = await Reservation.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Reservation not Found");
+      }
+    } catch (error) {
+      console.log("error updating data", error);
+    }
+  };
+  
+  export const deleteReservation = async (authorId: Number) => {
+    try {
+      const author = await Reservation.findByPk(authorId);
+      if (author) {
+        await author.destroy();
+        const authors = await Reservation.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Author not Found");
+      }
+    } catch (error) {
+      console.log("error deleteing data", error);
+    }
+  };
+  
