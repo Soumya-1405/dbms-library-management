47b576724e187c2041cd042ce4bb852ec0897abd
katta soumya <kattasoumya2002@gmail.com>
2024-07-05 12:11:37 +0530

    perform crud operations on members table

--- Repository/member.repository.js
+++ Repository/member.repository.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertMembersData = void 0;
+exports.deleteMember = exports.updateMember = exports.getAllMembers = exports.insertMembersData = void 0;
 const member_1 = require("../models/member");
 const data_1 = require("../staticData/data");
 const insertMembersData = () => __awaiter(void 0, void 0, void 0, function* () {
@@ -17,3 +17,47 @@ const insertMembersData = () => __awaiter(void 0, void 0, void 0, function* () {
     console.log("insert succesffullt");
 });
 exports.insertMembersData = insertMembersData;
+const getAllMembers = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const authors = yield member_1.Member.findAll();
+        console.table(authors.map((author) => author.toJSON()));
+    }
+    catch (error) {
+        console.log("error fetchiing authors", error);
+    }
+});
+exports.getAllMembers = getAllMembers;
+const updateMember = (authorId, updatedData) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield member_1.Member.findByPk(authorId);
+        if (author) {
+            yield author.update(updatedData);
+            const authors = yield member_1.Member.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Member not Found");
+        }
+    }
+    catch (error) {
+        console.log("error updating data", error);
+    }
+});
+exports.updateMember = updateMember;
+const deleteMember = (authorId) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const author = yield member_1.Member.findByPk(authorId);
+        if (author) {
+            yield author.destroy();
+            const authors = yield member_1.Member.findAll();
+            console.table(authors.map((author) => author.toJSON()));
+        }
+        else {
+            console.log("Member not Found");
+        }
+    }
+    catch (error) {
+        console.log("error deleteing data", error);
+    }
+});
+exports.deleteMember = deleteMember;
--- Repository/member.repository.ts
+++ Repository/member.repository.ts
@@ -4,3 +4,43 @@ export const insertMembersData = async () => {
     const member = await Member.bulkCreate(Data.memberData);
     console.log("insert succesffullt");
   };
+
+  export const getAllMembers = async () => {
+    try {
+      const authors = await Member.findAll();
+      console.table(authors.map((author: any) => author.toJSON()));
+    } catch (error) {
+      console.log("error fetchiing authors", error);
+    }
+  };
+  
+  export const updateMember = async (authorId: Number, updatedData: Object) => {
+    try {
+      const author = await Member.findByPk(authorId);
+      if (author) {
+        await author.update(updatedData);
+        const authors = await Member.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Member not Found");
+      }
+    } catch (error) {
+      console.log("error updating data", error);
+    }
+  };
+  
+  export const deleteMember = async (authorId: Number) => {
+    try {
+      const author = await Member.findByPk(authorId);
+      if (author) {
+        await author.destroy();
+        const authors = await Member.findAll();
+        console.table(authors.map((author: any) => author.toJSON()));
+      } else {
+        console.log("Member not Found");
+      }
+    } catch (error) {
+      console.log("error deleteing data", error);
+    }
+  };
+  
