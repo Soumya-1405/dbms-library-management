422d1b3bf4b9b6d514995b40a581cbae78ecd3b1
katta soumya <kattasoumya2002@gmail.com>
2024-07-09 17:49:58 +0530

    update the index file

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { sequelize } from "./db_connection/config";
-import router from "./Repository/author.repository";
-import bookRouter from "./Repository/book.repository";
-import loanRouter from "./Repository/loan.repository";
+import router, { insertAuthorsData } from "./Repository/author.repository";
+import bookRouter, { insertBooksData } from "./Repository/book.repository";
+import loanRouter, { insertLoansData }  from "./Repository/loan.repository";
 import { Book } from "./models/book";
 import { Loan } from "./models/loan";
 import { Member } from "./models/member";
@@ -13,8 +13,10 @@ import express from 'express';
 const app = express();
 // import { deleteMember, getAllMembers, insertMembersData, updateMember } from "./Repository/member.repository";
 // import { deleteLoan, getAllLoans, insertLoansData, updateLoan } from "./Repository/loan.repository";
-import { deleteReservation, getAllReservations, insertReservationData, updateReservation } from "./Repository/reservation.repository";
-import memberRouter from "./Repository/member.repository";
+// import { deleteReservation, getAllReservations, insertReservationData, updateReservation } from "./Repository/reservation.repository";
+import memberRouter, { insertMembersData } from "./Repository/member.repository";
+import reservationRouter, { insertReservationData } from "./Repository/reservation.repository";
+import { loadavg } from "os";
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded requests
 app.use(express.json()); // Middleware to parse JSON requests
 
@@ -22,8 +24,15 @@ async function initializeDatabase() {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-  
-    await sequelize.sync({alter: true});
+    // await Loan.destroy()
+    await sequelize.sync({force:true});
+    insertAuthorsData()
+    insertBooksData();
+    insertMembersData();
+    insertLoansData();
+    insertReservationData();
+    // await Reservation.sync({force:true});
+    // insertReservationData();
     console.log('All models were synchronized successfully.');
   } catch (error) {
     console.error('Unable to connect to the database or sync models:', error);
@@ -51,6 +60,7 @@ app.use('/api/authors',router);
 app.use('/api/books',bookRouter)
 app.use('/api/loans',loanRouter)
 app.use('/api/members',memberRouter)
+app.use('/api/reservations',reservationRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
