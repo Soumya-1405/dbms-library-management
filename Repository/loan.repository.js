219fde582b80ee2a4f7a0c66775f620e760b7544
katta soumya <kattasoumya2002@gmail.com>
2024-07-09 17:49:14 +0530

    create routes to perform crud operations on loans tables

--- Repository/loan.repository.js
+++ Repository/loan.repository.js
@@ -8,56 +8,85 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteLoan = exports.updateLoan = exports.getAllLoans = exports.insertLoansData = void 0;
+exports.insertLoansData = void 0;
 const loan_1 = require("../models/loan");
 const data_1 = require("../staticData/data");
+const express_1 = __importDefault(require("express"));
+const loanRouter = express_1.default.Router();
 const insertLoansData = () => __awaiter(void 0, void 0, void 0, function* () {
     const loans = yield loan_1.Loan.bulkCreate(data_1.Data.loansData);
     console.log('insert succesfully');
 });
 exports.insertLoansData = insertLoansData;
-const getAllLoans = () => __awaiter(void 0, void 0, void 0, function* () {
+(0, exports.insertLoansData)();
+loanRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const authors = yield loan_1.Loan.findAll();
-        console.table(authors.map((author) => author.toJSON()));
+        // Fetch all loans include books
+        const loans = yield loan_1.Loan.findAll();
+        if (loans.length === 0)
+            return res.status(404).json({ message: "No loans Found" });
+        res.json({ Loans: loans });
     }
-    catch (error) {
-        console.log("error fetchiing authors", error);
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
-});
-exports.getAllLoans = getAllLoans;
-const updateLoan = (authorId, updatedData) => __awaiter(void 0, void 0, void 0, function* () {
+}));
+// Get one loan
+loanRouter.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const author = yield loan_1.Loan.findByPk(authorId);
-        if (author) {
-            yield author.update(updatedData);
-            const authors = yield loan_1.Loan.findAll();
-            console.table(authors.map((author) => author.toJSON()));
+        const loan = yield loan_1.Loan.findByPk(req.params.id);
+        if (loan === null) {
+            return res.status(404).json({ message: "Loan Not Found" });
+        }
+        res.json(loan);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}));
+// Create a new loan
+loanRouter.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const loan = yield loan_1.Loan.create(req.body);
+        res.json(loan);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}));
+// Update an loan
+loanRouter.put('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const [updated] = yield loan_1.Loan.update(req.body, { where: { id: req.params.id } });
+        if (updated) {
+            const updatedAuthor = yield loan_1.Loan.findByPk(req.params.id);
+            res.json(updatedAuthor);
         }
         else {
-            console.log("Loan not Found");
+            res.status(404).json({ message: "Loan Not Found" });
         }
     }
-    catch (error) {
-        console.log("error updating data", error);
+    catch (err) {
+        res.status(400).json({ message: err.message });
     }
-});
-exports.updateLoan = updateLoan;
-const deleteLoan = (authorId) => __awaiter(void 0, void 0, void 0, function* () {
+}));
+// Delete an loan
+loanRouter.delete('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const author = yield loan_1.Loan.findByPk(authorId);
-        if (author) {
-            yield author.destroy();
-            const authors = yield loan_1.Loan.findAll();
-            console.table(authors.map((author) => author.toJSON()));
+        const deleted = yield loan_1.Loan.destroy({ where: { id: req.params.id } });
+        if (deleted) {
+            res.json({ message: "Loan Deleted" });
         }
         else {
-            console.log("Loan not Found");
+            res.status(404).json({ message: "Loan Not Found" });
         }
     }
-    catch (error) {
-        console.log("error deleteing data", error);
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
-});
-exports.deleteLoan = deleteLoan;
+}));
+exports.default = loanRouter;
--- Repository/loan.repository.ts
+++ Repository/loan.repository.ts
@@ -1,46 +1,72 @@
 import { Loan } from "../models/loan";
 import { Data } from "../staticData/data";
+import express from 'express';
+const loanRouter = express.Router();
 export const insertLoansData = async () => {
     const loans = await Loan.bulkCreate(Data.loansData);
     console.log('insert succesfully')
   };
+  insertLoansData()
   
-  export const getAllLoans = async () => {
+  loanRouter.get('/', async (req, res) => {
     try {
-      const authors = await Loan.findAll();
-      console.table(authors.map((author: any) => author.toJSON()));
-    } catch (error) {
-      console.log("error fetchiing authors", error);
+        // Fetch all loans include books
+        const loans = await Loan.findAll();
+        if (loans.length === 0) return res.status(404).json({ message: "No loans Found" });
+        res.json({Loans: loans});
+    } catch (err:any) {
+        res.status(500).json({message: err.message});
     }
-  };
-  
-  export const updateLoan = async (authorId: Number, updatedData: Object) => {
+  });
+  // Get one loan
+  loanRouter.get('/:id', async (req, res) => {
     try {
-      const author = await Loan.findByPk(authorId);
-      if (author) {
-        await author.update(updatedData);
-        const authors = await Loan.findAll();
-        console.table(authors.map((author: any) => author.toJSON()));
-      } else {
-        console.log("Loan not Found");
-      }
-    } catch (error) {
-      console.log("error updating data", error);
+        const loan = await Loan.findByPk(req.params.id);
+        if (loan === null) {
+            return res.status(404).json({ message: "Loan Not Found" });
+        }
+        res.json(loan);
+    } catch (err:any) {
+        res.status(500).json({message: err.message});
     }
-  };
+  });
   
-  export const deleteLoan = async (authorId: Number) => {
+  // Create a new loan
+  loanRouter.post('/', async (req, res) => {
     try {
-      const author = await Loan.findByPk(authorId);
-      if (author) {
-        await author.destroy();
-        const authors = await Loan.findAll();
-        console.table(authors.map((author: any) => author.toJSON()));
-      } else {
-        console.log("Loan not Found");
-      }
-    } catch (error) {
-      console.log("error deleteing data", error);
+        const loan = await Loan.create(req.body);
+        res.json(loan);
+    } catch (err:any) {
+        res.status(400).json({message: err.message});
     }
-  };
+  });
   
+  // Update an loan
+  loanRouter.put('/:id', async (req, res) => {
+    try {
+        const [updated] = await Loan.update(req.body, {where: {id: req.params.id}});
+        if (updated) {
+            const updatedAuthor = await Loan.findByPk(req.params.id);
+            res.json(updatedAuthor);
+        } else {
+            res.status(404).json({ message: "Loan Not Found" });
+        }
+    } catch (err:any) {
+        res.status(400).json({message: err.message});
+    }
+  });
+  // Delete an loan
+  loanRouter.delete('/:id', async (req, res) => {
+    try {
+        const deleted = await Loan.destroy({where: {id: req.params.id}});
+        if (deleted) {
+            res.json({ message: "Loan Deleted" });
+        } else {
+            res.status(404).json({ message: "Loan Not Found" });
+        }
+    } catch (err:any) {
+        res.status(500).json({message: err.message});
+    }
+  });
+  
+  export default loanRouter;
